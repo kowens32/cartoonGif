05613a7ae16e49355751183f441cc1a6de02cb25
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-09-12 15:35:03 -0400

    added new characters

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
         for (var i = 0; i < cartoonChar.length; i++) {
             var b = $('<button>');
             b.attr('data-name', cartoonChar[i]);
+            b.attr('class', 'character');
             b.text(cartoonChar[i]);
             b.appendTo('.cartoon-buttons')
         }
@@ -24,58 +25,71 @@ $(document).ready(function () {
 
     cartoonButton();
 
-    $(document).on('click', 'button', function () {
-        $('.cartoon-display').empty();
-        var cartoonGif = $(this).attr('data-name');
-        console.log('the name is ' + this);
 
-        var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + cartoonGif + '&api_key=dc6zaTOxFJmzC';
-        $.ajax({url: queryURL, method: 'GET'})
-            .done(function (response) {
-                console.log(response);
-                var gifResults = response.data;
-                console.log('where am i ' + gifResults);
-                for (var i = 0; i < 10; i++) {
-                    if (gifResults[i].rating !== 'r' && gifResults[i].rating !== 'pg-13') {
-                        var gifImage = $('<img>');
-                        gifImage.attr({
-                            'src': gifResults[i].images.fixed_height_still.url,
-                            'data-animate': gifResults[i].images.fixed_height.url,
-                            'data-still': gifResults[i].images.fixed_height_still.url,
-                            'data-state': 'still'
-                        });
+    function displayCartoon() {
+        $(document).on('click', '.cartoon-buttons .character', function () {
+            $('.cartoon-display').empty();
+            var cartoonGif = $(this).attr('data-name');
+            console.log('the name is ' + this);
+            var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + cartoonGif + '&api_key=dc6zaTOxFJmzC';
+            $.ajax({url: queryURL, method: 'GET'})
+                .done(function (response) {
+                    console.log(response);
+                    var gifResults = response.data;
+                    console.log('where am i ' + gifResults);
+                    for (var i = 0; i < 10; i++) {
+                        if (gifResults[i].rating !== 'r' && gifResults[i].rating !== 'pg-13') {
+                            var gifImage = $('<img>');
+                            gifImage.attr({
+                                'src': gifResults[i].images.fixed_height_still.url,
+                                'data-animate': gifResults[i].images.fixed_height.url,
+                                'data-still': gifResults[i].images.fixed_height_still.url,
+                                'data-state': 'still'
+                            });
 
-                        $('.cartoon-display').append(gifImage);
+                            $('.cartoon-display').append(gifImage);
 
+                        }
                     }
-                }
-            });
-    });
+                });
+        });
+    }
+    displayCartoon();
 
-    $('#search').on('click', function (event) {
-        var newChar = $('#user-character').val();
-        console.log(newChar);
-        // The Cartoon Character from the textbox is then added to our array
-        if (newChar !== '') {
-            cartoonChar.push(newChar);
-            //  $('#user-character').val('');
-        }
-    })
+//Adds character to the Array
+    function addChar() {
+        $(document).on('click', '#search', function (event) {
+            var newChar = $('#user-character').val();
+            console.log(newChar);
+            if (newChar !== '') {
+                cartoonChar.push(newChar);
+                console.log(cartoonChar);
+                cartoonButton();
+                displayCartoon();
+            }
+        })
+    }
+    addChar();
 
 
-    $(document).on('click', 'img', function () {
-        var state = $(this).attr('data-state');
-        console.log('who ' + state);
-        if (state === 'still') {
-            $(this).attr('src', $(this).attr('data-animate'));
-            $(this).attr('data-state', 'animate');
-        }
+//Animates the gifs once they are clicked
+    function animate() {
+        $(document).on('click', 'img', function () {
+            var state = $(this).attr('data-state');
+            console.log('who ' + state);
+            if (state === 'still') {
+                $(this).attr('src', $(this).attr('data-animate'));
+                $(this).attr('data-state', 'animate');
+            }
 
-        else {
-            $(this).attr('src', $(this).attr('data-still'));
-            $(this).attr('data-state', 'still');
-        }
-    });
+            else {
+                $(this).attr('src', $(this).attr('data-still'));
+                $(this).attr('data-state', 'still');
+            }
+        });
+
+    }
+    animate();
 
 
 
